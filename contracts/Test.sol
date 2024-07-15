59b8c40f71a7c1cfc58ba555674a6418f16c785c
Rita Tcepeleva <tsepeleva.rita@mail.ru>
2024-07-15 19:06:07 +0300

    remove unnecessary files

--- contracts/Test.sol
+++
@@ -1,2 +0,0 @@
-import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
-import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
